bc4380fe05639be4338c79f3996e3d272a6d197c
Nagy Barnabas <nagybarnabas@Nagy-MacBook-Pro.local>
2025-03-31 15:48:06 +0200

    Some small update on homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,22 @@ const Home = () => {
 
     const {allCoin, currency} = useContext(CoinContext);
     const [displayCoin, setDisplayCoin] = useState([]);
+    const [input, setInput] = useState('');
+
+    const inputHandler = (event) => {
+        setInput(event.target.value);
+        if (event.target.value === "") {
+            setDisplayCoin(allCoin);
+        }
+    }
+
+    const searchHandler = async (event) => {
+        event.preventDefault();
+        const coins = await allCoin.filter((item)=>{
+            return item.name.toLowerCase().includes(input.toLowerCase())
+        })
+        setDisplayCoin(coins)
+    }
 
     useEffect(() =>{
         setDisplayCoin(allCoin);
@@ -16,8 +32,13 @@ const Home = () => {
         <div className="hero">
             <h1>Crypto <br /> Price Tracker</h1>
             <p>Welcome here</p>
-            <form>
-                <input type="text" placeholder='Search crypto..' />
+            <form onSubmit={searchHandler}>
+                <input onChange={inputHandler} value={input} list='coinlist' type="text" placeholder='Search crypto..' required />
+
+                <datalist id='coinlist'>
+                    {allCoin.map((item,index)=>(<option key={index} value={item.name}/>))}
+                </datalist>
+
                 <button type='submit'>Search</button>
             </form>
         </div>
