f60489f45d5f2a8fca552362b8300d99dba35294
Nagy Barnabas <nagybarnabas@Nagy-MacBook-Pro.local>
2025-03-31 14:31:17 +0200

    Making homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,24 @@ import './Home.css'
 
 const Home = () => {
   return (
-    <div>
-
+    <div className='home'>
+        <div className="hero">
+            <h1>Crypto <br /> Price Tracker</h1>
+            <p>Welcome here</p>
+            <form>
+                <input type="text" placeholder='Search crypto..' />
+                <button type='submit'>Search</button>
+            </form>
+        </div>
+        <div className="crypto-table">
+            <div className="table-layout">
+                <p>#</p>
+                <p>Coins</p>
+                <p>Price</p>
+                <p style={{ textAlign:"center"}}>24H Change</p>
+                <p className='marketcap'>Market cap</p>
+            </div>
+        </div>
     </div>
   )
 }
